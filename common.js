165b0f9bafbbec0489f9ffdadecc214f81510f6d
Jsh <hajsh930@gmail.com>
2025-05-14 14:43:55 +0900

    feat : 가이드맵 처리

--- common.js
+++ common.js
@@ -16,4 +16,24 @@ $(function() {
 					$(this).prop('href', $(v).text());
 				});
 			});
+
+		
+	    $.getJSON('/MenuList.json', function(data) {
+	      const html = undefined || [];
+				
+	      $.each(data, function(i, item) {
+					html.push('<div class="menuList">');
+					html.push('<dl>');
+					html.push('<dt>' + item.menuName + '</dt>');
+					html.push('<dd>' + item.depth1 + '</dd>');
+					html.push('<dd>' + item.depth2 + '</dd>');
+					html.push('<dd><a href='+ item.devUrl +' class="dev" target="_blank">개발</a></dd>');
+					html.push('<dd><a href='+ item.mainUrl +' class="main" target="_blank">운영</a></dd>');
+					html.push('<dd>' + item.update + '</dd>');
+					html.push('</dl>');
+					html.push('</div>');
+	      });
+	      // console.log(html);
+	      $('.traget').html(html.join(''));
+	    });
 	});

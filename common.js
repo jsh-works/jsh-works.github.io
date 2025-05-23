bd0261188805613807edf2242b772c173eebc569
DESKTOP-309P4G8\SR91 <hajsh930@gmail.com>
2025-05-23 16:42:33 +0900

    feat : 페이지 분리, list 최신화

--- common.js
+++ common.js
@@ -1,44 +1,24 @@
 $(function() {
-			var filter = 
-			$.each($("td.url > a"), function(i,v){
-				$(v).text($(v).attr("href"));
-			});
-
-			$(".kor").on('click', function() {
-				$.each($("td.url > a"), function(i,v){
-					$(v).html($(v).attr("href").replace('./en','./kr'));
-					$(this).prop('href', $(v).text());
-				});
-			});
-			
-			$(".en").on('click', function() {
-				$.each($("td.url > a"), function(i,v){
-					$(v).html($(v).attr("href").replace('./kr','./en'));
-					$(this).prop('href', $(v).text());
-				});
-			});
-
-		
-	    $.getJSON('/MenuList.json', function(data) {
+	    $.getJSON('./MenuList.json', function(data) {
 	      const html = undefined || [];
+	      const div = undefined || [];
 				
 	      $.each(data, function(i, item) {
 					html.push('<div class="menuList">');
 					html.push('<dl>');
 					html.push('<dt>' + item.menuName + '</dt>');
-					html.push('<dd>' + item.depth1 + '</dd>');
-					html.push('<dd>' + item.depth2 + '</dd>');
 					html.push('<dd><a href='+ item.devUrl +' class="dev" target="_blank">개발</a></dd>');
 					if(!item.mainUrl && item.mainUrl === "") {
 						html.push('<dd></dd>');
 					} else {
 						html.push('<dd><a href='+ item.mainUrl +' class="main" target="_blank">운영</a></dd>');
 					}
-					html.push('<dd>' + item.update + '</dd>');
+					html.push('<dd >' + item.update + '</dd>');
 					html.push('</dl>');
 					html.push('</div>');
 	      });
 	      // console.log(html);
+	      $('.sort').html(div.join(''));
 	      $('.traget').html(html.join(''));
 	    });
 	});

add566851707f246a2989dc7a5caa4bb59e9bf59
DESKTOP-309P4G8\SR91 <hajsh930@gmail.com>
2025-06-13 16:03:53 +0900

    feat : prj, 전체수정

--- common.js
+++ common.js
@@ -6,14 +6,21 @@ $(function() {
 	      $.each(data, function(i, item) {
 					html.push('<div class="menuList">');
 					html.push('<dl>');
-					html.push('<dt>' + item.menuName + '</dt>');
-					html.push('<dd><a href='+ item.devUrl +' class="dev" target="_blank">개발</a></dd>');
-					if(!item.mainUrl && item.mainUrl === "") {
-						html.push('<dd></dd>');
-					} else {
-						html.push('<dd><a href='+ item.mainUrl +' class="main" target="_blank">운영</a></dd>');
+					if(item.id === "bookStory") {
+						html.push('<dt>📖</dt>');
+					} else if(item.id === "e-book") {
+						html.push('<dt>💻</dt>');
 					}
-					html.push('<dd >' + item.update + '</dd>');
+
+					
+					html.push('<dd class="menu">' + item.menuName + '</dd>');
+					html.push('<dd style="width: 20%;"><a href='+ item.devUrl +' class="dev" target="_blank">개발</a></dd>');
+					if(!item.mainUrl && item.mainUrl === "") {
+						html.push('<dd style="width: 20%;"></dd>');
+					} else {
+						html.push('<dd style="width: 20%;"><a href='+ item.mainUrl +' class="main" target="_blank">운영</a></dd>');
+					}
+					html.push('<dd style="width: 20%;" >' + item.update + '</dd>');
 					html.push('</dl>');
 					html.push('</div>');
 	      });
--- prj-react-app/src/components/Wrapper/Wrapper.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Wrapper.module.css';
-
-export default function Wrapper({ children }) {
-  return (
-    <div className={styles.wrapper}>
-      { children }
-    </div>
-  )
-}

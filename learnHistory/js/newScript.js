0aec68e5a77e2164b9a360e43100f2c86076e9b9
DESKTOP-309P4G8\SR91 <hajsh930@gmail.com>
2025-08-04 13:45:46 +0900

    feat : prj, learnHistory - 이력상세현황, 학급생성 및 현황 퍼블리싱 완료

--- learnHistory/js/newScript.js
+++ learnHistory/js/newScript.js
@@ -15,38 +15,57 @@ $(function() {
     }
   });
 
-  // 나의 학습이력 관리 - 교재열람 progress bar
-  var currentValue = $(".value .current-val").text(); // 현재 학습페이지
-  var totalValue = $(".value .total-val").text(); // 전체 페이지
-  var realScore = (currentValue / totalValue) * 100; // 실제 백분율 값
-  // console.log(realScore);
-
-  $(".progress").width(totalValue); // 프로그래스바 전체 값
-  $(".progress .score").width(realScore); // 프로그래스바 현재 값
-
-  if(realScore == 100) {
-    $(".value .current-val").css('color', '#F55149');
-    $(".progress .score").css('background-color', '#F55149');
-  }
-
-  // 나의 학습이력 관리 - 문제풀이
-  var currentProblem = $(".problem .current-val").text(); // 현재 학습페이지
-  var totalProblem = $(".problem .total-val").text(); // 전체 페이지
-
-  if(currentProblem === totalProblem) {
-    $(".problem .current-val").css({
-      'color' : '#444',
-      'font-weight' : '400'
-    });
-    $(".problem-status").text("완료");
-  } else {
-    $(".problem-status").text("진행중");
-  }
-
+  // 나의 학습이력 관리 - 학습이력 동의여부
   const toggleList = document.querySelectorAll(".switch");
-    toggleList.forEach(($toggle) => {
-      $toggle.onclick = () => {
-        $toggle.classList.toggle('active');
-      }
-    });
+  toggleList.forEach(($toggle) => {
+    $toggle.onclick = () => {
+      $toggle.classList.toggle('active');
+    }
+  });
+
+  // info 버튼 말풍선 스크립트
+  $(".info").on('click', function() {
+    var clickPosition = $(this).offset();
+    var thisLeft = $(this).position().left;
+    if(!$(".info").hasClass('active')) {
+      $(this).addClass('active');
+      $('.helpTip').show();
+      $('.helpTip').css({
+        top: clickPosition.top + $(this).outerHeight(),
+        left : thisLeft - 35
+      })
+    } else {
+      $(".info").removeClass('active');
+      $('.helpTip').hide();
+    }
+  });
+
+  // 나의 학습이력 관리 - 교재열람 progress bar
+  // var currentValue = $(".value .current-val").text(); // 현재 학습페이지
+  // var totalValue = $(".value .total-val").text(); // 전체 페이지
+  // var realScore = (currentValue / totalValue) * 100; // 실제 백분율 값
+
+  // $(".progress").width(totalValue); // 프로그래스바 전체 값
+  // $(".progress .score").width(realScore); // 프로그래스바 현재 값
+
+  // if(realScore == 100) {
+  //   $(".value .current-val").css('color', '#F55149');
+  //   $(".progress .score").css('background-color', '#F55149');
+  // }
+
+  // // 나의 학습이력 관리 - 문제풀이
+  // var currentProblem = $(".problem .current-val").text(); // 현재 학습페이지
+  // var totalProblem = $(".problem .total-val").text(); // 전체 페이지
+
+  // if(currentProblem === totalProblem) {
+  //   $(".problem .current-val").css({
+  //     'color' : '#444',
+  //     'font-weight' : '400'
+  //   });
+  //   $(".problem-status").text("완료");
+  // } else {
+  //   $(".problem-status").text("진행중");
+  // }
+
+  
 });

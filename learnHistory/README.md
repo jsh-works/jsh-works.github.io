cb864ec1526ac637270018591839653ce498467a
DESKTOP-309P4G8\SR91 <hajsh930@gmail.com>
2025-08-22 13:22:13 +0900

    feat : learnHistory - 학급시스템관리 (tablet Ver) 퍼블리싱 완료 + js 추가완료

--- learnHistory/README.md
+++ learnHistory/README.md
@@ -4,7 +4,7 @@
 |:---|---:|
 | 동의 팝업 | [agreePopup.html](agreePopup.html) |
 | 상세현황 | [detailStatus.html](detailStatus.html) |
-| 학급생성 및 현황 | [classStatus.html](classStatus.html) |
+| 학급별 학습이력 관리 | [classStatus.html](classStatus.html) |
 | 신규 학급 생성 | [createClass.html](createClass.html) |
 | 학생별 학습 이력 | [learningHistory.html](learningHistory.html) |
 | 결과지 다운로드 팝업 | [resultPopup.html](resultPopup.html) |
--- learnHistory/js/newScript.js
+++ learnHistory/js/newScript.js
@@ -1,15 +1,5 @@
 $(function() {
-
-  // 동의 팝업 - 동의하기
-  $(".invite-popup .agree-btn").on('click', function() {
-    location.href = '../learnHistory/detailStatus.html';
-  });
-
-  // 동의 팝업 - 거절하기
-  $(".invite-popup .close, .invite-popup .cancel-btn").on('click', function() {
-    window.close();
-  });
-
+  // 앱 다운로드 - 앨범/목록형 타입 선택
   $(".types button").on('click', function() {
     if($(this).hasClass('album-type')) {
       $('.album-type').addClass('on');
@@ -25,6 +15,16 @@ $(function() {
     }
   });
 
+  // 학급시스템 초대하기 (동의) - 팝업 동의하기 선택
+  $(".invite-popup .agree-btn").on('click', function() {
+    location.href = '../learnHistory/detailStatus.html';
+  });
+
+  // 학급시스템 초대하기 (동의) - 팝업 거절하기 선택
+  $(".invite-popup .close, .invite-popup .cancel-btn").on('click', function() {
+    window.close();
+  });
+
   // 나의 학습이력 관리 - 학습이력 동의여부
   const toggleList = document.querySelectorAll(".switch");
   toggleList.forEach(($toggle) => {
@@ -38,21 +38,7 @@ $(function() {
     }
   });
 
-  // 학습결과 보고서 열기
-  $(".goDetails").on('click', function() {
-    window.open(
-      "../learnHistory/resultPopup.html",
-      "학습결과 보고서",
-      "width=1100,height=812, left=" + (window.screen.width - 1800) / 2 + ",top=100"
-    );
-  });
-
-  // 학습결과 보고서 닫기
-  $(".result-popup .close").on('click', function() {
-    window.close();
-  })
-
-  // info 버튼 말풍선 스크립트
+  // 공통 - info 버튼 말풍선
   $(".info").on('click', function() {
     var clickPosition = $(this).offset();
     var thisLeft = $(this).position().left;
@@ -69,7 +55,77 @@ $(function() {
     }
   });
 
-  // 학생별/교재별 학습이력 선택
+
+  // 학급별 학습이력 관리 - 학급생성
+  $(".class-box.create").on('click', function() {
+    location.href = '../learnHistory/createClass.html';
+  });
+
+  // 학급별 학습이력 관리 - 학급삭제
+  $(".class-delete-ico").on('click', function(e) {
+    e.preventDefault();
+    var student = false; // 연계된 학생 임시 변수 true, false
+
+    if(student) { // 연계된 학생이 있을 경우
+      if(confirm("선택한 학급이 삭제 됩니다. 삭제하겠습니까?")) {
+        alert("삭제되었습니다.");
+      }
+      
+    } else { // 연계된 학생이 없을 경우
+      if(confirm("학급 삭제 시,\n연계된 학생 내역도 사라지며 삭제된 내역은 복구되지 않습니다.\n그래도 삭제하시겠습니까?")) {
+        alert("삭제되었습니다.");
+      }
+    }
+  });
+
+  // 학급별 학습이력 관리 - 학급 정보 수정
+  $(".create-class-btn").on('click', function(e) {
+    e.preventDefault();
+    location.href = '../learnHistory/createClass.html';
+  });
+
+  // 학급별 학습이력 관리 - 학급 초대하기 (APP 기능)
+  $(".class-invite-btn").on('click', function(e) {
+    e.preventDefault();
+    alert("카카오톡 초대하기");
+  });
+
+  // 학급별 학습이력 관리 - 학급 복사하기
+  $(".class-copy-btn").on('click', function(e) {
+    e.preventDefault();
+    var copyUrl = 'https://jsh-works.github.io/learnHistory/detailStatus.html';
+    window.navigator.clipboard.writeText(copyUrl).then(() => {
+      alert("링크 URL이 복사 되었습니다.");
+    });
+  });
+
+  // 학급별 학습이력 관리 - 교사 인증 받기
+  $(".teacher-cert-btn").on('click', function(e) {
+    e.preventDefault();
+    window.open('https://sso.ebs.co.kr/user/auth', target='_blank');
+
+  });
+
+  // 신규 학급 생성 - 배경색상 선택
+  $(".color-box ul li button").on('click', function() {
+    if(!$(this).hasClass('acitve')) {
+      $(".color-box ul li button").removeClass('active');
+      $(this).addClass('active');
+    }
+  });
+
+  // 신규 학급 생성 - 취소 버튼
+  $(".class .cancel-btn").on('click', function() {
+    location.href = '../learnHistory/classStatus.html';
+  });
+
+  // 신규 학급 생성 - 저장 버튼
+  $(".class .save-btn").on('click', function() {
+    alert("저장되었습니다.");
+    location.href = '../learnHistory/detailStatus.html';
+  });
+
+  // 학생별 학습이력 - 학습이력 탭 선택
   $(".tab-con button").on('click', function() {
     if(!$(this).hasClass('acitve')) {
       $(".tab-con button").removeClass('active');
@@ -97,30 +153,157 @@ $(function() {
     }
   });
 
+  // 학생별 학습이력 - 학생 이름 삭제
+  $(".name-del-btn").on('click', function() {
+    if(confirm("해당 학생을 삭제하시겠습니까?\n학생 삭제 시, 학생의 학습 이력도 사라지며 삭제된 내역은 복구되지 않습니다.")) {
+      alert("삭제되었습니다.");
+    }
+  });
 
-  // 별명 팝업 열기
+  // 학생별 학습이력 - 별명 팝업 열기
   $(".alias-btn").on('click', function() {
     $(".alias-popup").css('display', 'flex');
   });
 
-  // 별명 팝업 닫기
+  // 학생별 학습이력 - 별명 팝업 닫기
   $(".alias-popup .close").on('click', function() {
     $(".alias-popup").hide();
   });
 
+  // 학생별 학습이력 - 별명 저장
   var text = '금칙어';
-  $(".save-btn").on('click', function() {
+  $(".alias-popup .save-btn").on('click', function() {
     if(text == '금칙어') {
-      alert("금칙어가 포함되어있습니다.");
+      alert("금칙어 또는 실명이 포함되어있습니다.");
     } else {
-      alert("저장이 완료되었습니다.");
+      alert("저장되었습니다.");
     }
   });
 
-  $(".clear-btn").on('click', function() {
-    alert("변경되었습니다.");
+  // 학생별 학습이력 - 별명 확인
+  $(".alias-popup .clear-btn").on('click', function() {
+    alert("저장되었습니다.");
     $(".alias-popup").css('display', 'none');
   });
 
+  // 학습결과 보고서 (결과지 다운로드) - 도넛 차트 (열람 률)
+  function initializeChart1() {
+    var newCtx1 = $("#newDoughnut1");
+    var newDoughnut1 = new Chart(newCtx1, {
+      type: 'doughnut',
+      data: {
+        datasets: [{
+          data: [100, 0], // 1.(실제값(100)) 2.(최대값(100) - 실제값(100) = 남은값(0))
+          backgroundColor: ['#2BC1EA', '#EEF1F8'],
+          borderColor: ['#2BC1EA', '#EEF1F8'],
+          borderWidth: 1,
+          cutout: "80%",
+        }]
+      },
+      options: {
+        plugins: {
+          annotation: {
+            annotations: {
+              centerLabel: {
+                type: 'doughnutLabel',
+                content: ({chart}) => [
+                  chart.data.datasets[0].data[0] + '%' // 현재값 표시
+                ],
+                font: { size: 36, weight : 700 }
+              }
+            }
+          }
+        },
+        responsive: false,
+        cutoutPercentage: 80,
+        tooltips: { enabled: false },
+        events: [],
+      },
+    });
+  }
   
+  // 학습결과 보고서 (결과지 다운로드) - 도넛 차트 (풀이 율)
+  function initializeChart2() {
+    var newCtx2 = $("#newDoughnut2");
+    var newDoughnut2 = new Chart(newCtx2, {
+      type: 'doughnut',
+      data: {
+        datasets: [{
+          data: [70, 30], // 1.(실제값(70)) 2.(최대값(100) - 실제값(70) = 남은값(30))
+          backgroundColor: ['#4165F4', '#EEF1F8'],
+          borderColor: ['#4165F4', '#EEF1F8'],
+          borderWidth: 1,
+          cutout: "80%",
+        }]
+      },
+      options: {
+        plugins: {
+          annotation: {
+            annotations: {
+              centerLabel: {
+                type: 'doughnutLabel',
+                content: ({chart}) => [
+                  chart.data.datasets[0].data[0] + '%' // 현재값 표시
+                ],
+                font: { size: 36, weight : 700 }
+              }
+            }
+          }
+        },
+        responsive: false,
+        cutoutPercentage: 80,
+        tooltips: { enabled: false },
+        events: [],
+      },
+    });
+  }
+
+  // 학습결과 보고서 (결과지 다운로드) - 도넛 차트 (최종,최초 정답률)
+  function initializeChart3() {
+    var newCtx3 = $("#newDoughnut3");
+    var newDoughnut3 = new Chart(newCtx3, {
+      type: 'doughnut',
+      data: {
+        datasets: [{
+          data: [40, 68], // 1.(실제값(40)) 2.(최대값(100) - 실제값(40) = 남은값(60))
+          backgroundColor: ['#A041F4', '#EEF1F8'],
+          borderColor: ['#A041F4', '#EEF1F8'],
+          borderWidth: 1,
+          cutout: "80%",
+        }]
+      },
+      options: {
+        plugins: {
+          annotation: {
+            annotations: {
+              centerLabel: {
+                type: 'doughnutLabel',
+                content: ({chart}) => [
+                  chart.data.datasets[0].data[0] + '%' // 현재값 표시
+                ],
+                font: { size: 36, weight : 700 }
+              }
+            }
+          }
+        },
+        responsive: false,
+        cutoutPercentage: 80,
+        tooltips: { enabled: false },
+        events: [],
+      },
+    });
+  }
+
+  // 학습결과 보고서 (결과지 다운로드) - 팝업 열기
+  $(".goDetails").on('click', function() {
+    $(".result-popup").css('display', 'flex');
+    initializeChart1(); // 열람 률 도넛 차트
+    initializeChart2(); // 풀이 율 도넛 차트
+    initializeChart3(); // 최종(최초) 정답률 도넛 차트
+  });
+
+  // 학습결과 보고서 (결과지 다운로드) - 팝업 닫기
+  $(".result-popup .close").on('click', function() {
+    $(".result-popup").hide();
+  })
 });
